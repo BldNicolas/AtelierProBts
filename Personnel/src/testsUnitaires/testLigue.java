4ac878437d03a7fdbc01bc008dc4e6b8edc98b1d
Nicolas <ni.berlaud@gmail.com>
2024-01-18 23:45:39 +0100

    Add New Test

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -1,14 +1,15 @@
 package testsUnitaires;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 
 import personnel.*;
 
-class testLigue 
+class testLigue
 {
 	GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
-	
+
 	@Test
 	void createLigue() throws SauvegardeImpossible
 	{
@@ -20,7 +21,42 @@ class testLigue
 	void addEmploye() throws SauvegardeImpossible
 	{
 		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
-		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty", LocalDate.now(), null);
 		assertEquals(employe, ligue.getEmployes().first());
+	}
+
+	@Test
+	void getNom() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Bowling");
+		assertEquals("Bowling", ligue.getNom());
+	}
+
+	@Test
+	void setNom() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Lancer de hache");
+		ligue.setNom("Lancer de hache sur cibles vivantes");
+		assertEquals("Lancer de hache sur cibles vivantes", ligue.getNom());
+	}
+
+	@Test
+	void getAdmin() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Barbecue");
+		//TODO : Ajouter les paramètres de dates
+		Employe employe = ligue.addEmploye("D'Arc", "Jeanne", "j.arc@weber.com", "gécho", LocalDate.now(), null);
+		ligue.setAdministrateur(employe);
+		assertEquals("D'Arc", ligue.getAdministrateur().getNom());
+	}
+
+	@Test
+	void setAdmin() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Tir à l'arc");
+		//TODO : Ajouter les paramètres de dates
+		Employe employe = ligue.addEmploye("DesBois", "Robin", "je.tire@pommes.com", "feu", LocalDate.now(), null);
+		ligue.setAdministrateur(employe);
+		assertEquals(employe, ligue.getAdministrateur());
 	}
 }
