5ed061240793fa2667bf6476edea0f474ca7caa5
Lav000 <77846416+Lav000@users.noreply.github.com>
2024-02-02 17:50:49 +0100

    🐛 fix: faute de frappe dans un @throws

--- Personnel/src/personnel/Employe.java
+++ Personnel/src/personnel/Employe.java
@@ -168,7 +168,7 @@ public class Employe implements Serializable, Comparable<Employe>
 	/**
 	 * Change la date d'arrivé de l'employé
 	 * @param dateArrive
-	 * @throws IllealArgumentException
+	 * @throws IllegalArgumentException
 	 */
 
 	public void setDateArrive(LocalDate dateArrive) {
