72ccf2774c1c25a679405aab2e052b7ee31ec4bc
Lav000 <77846416+Lav000@users.noreply.github.com>
2024-02-02 15:39:32 +0100

    🔧 Correction in the setUp

--- Personnel/src/testsUnitaires/testsEmploye.java
+++ Personnel/src/testsUnitaires/testsEmploye.java
@@ -2,67 +2,62 @@ package testsUnitaires;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import personnel.*;
 
 public class testsEmploye {
-	private Employe employe = new Employe();
-
+	
 	//Initiaisation d'un employe
-    @Before
-    public void setUp() {
-        employe = new Employe(
-                new GestionPersonnel(),
-                new Ligue("Elvis is the best"),
-                "TheRock",
-                "Ibrahimovic",
-                "zoumbacafe@pitch.com",
-                "original",
-                LocalDate.of(25, 1, 2002),
-                LocalDate.of(1, 10, 2002));
+	GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
+	static Employe employe;
+    public void setUp() throws SauvegardeImpossible {
+    	Ligue ligue = gestionPersonnel.addLigue("Panda2");
+    	Employe employe = ligue.addEmploye("Le Chant", "Léo", "leo@coucou.com", "mdp", LocalDate.of(1, 1, 2018), LocalDate.of(1, 1, 2020));
     }
 
     //Test des setters
     @Test
-    public void SetNom() throws SauvegardeImpossible{
-        employe.setNom("Loup");
+    void SetNom() throws SauvegardeImpossible{
+		employe.setNom("Loup");
         assertEquals("Loup", employe.getNom());
     }
 
     @Test
-    public void SetPrenom() throws SauvegardeImpossible{
+    void SetPrenom() throws SauvegardeImpossible{
         employe.setPrenom("Garou");
         assertEquals("Garou", employe.getPrenom());
     }
 
     @Test
-    public void SetMail() throws SauvegardeImpossible{
+    void SetMail() throws SauvegardeImpossible{
         employe.setMail("lemail@gmal.com");
         assertEquals("lemail@gmal.com", employe.getMail());
     }
 
     @Test
-    public void SetPassword() throws SauvegardeImpossible{
+    void SetPassword() throws SauvegardeImpossible{
         employe.setPassword("gngngn12!++A");
         assertTrue(employe.checkPassword("gngngn12!++A"));
     }
 
     @Test
-    public void SetDateArrive() throws SauvegardeImpossible{
+    void SetDateArrive() throws SauvegardeImpossible{
         LocalDate testDate = LocalDate.of(18, 1, 2024);
         employe.setDateArrive(testDate);
         assertEquals(testDate, employe.getDateArrive());
     }
 
     @Test
-    public void SetDateDepart() throws SauvegardeImpossible{
+    void SetDateDepart() throws SauvegardeImpossible{
         LocalDate nouvelleDateDepart = LocalDate.of(1, 1, 2026);
         employe.setDateDepart(nouvelleDateDepart);
         assertEquals(nouvelleDateDepart, employe.getDateDepart());
     }
     
-    //Test des exceptions
+    //Test des getters
 
 }
 
