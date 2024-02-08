f2238cdcc3da8365563f942c4f65310a7998a3f1
Djony-Herbaut <djonyandrejovinac@gmail.com>
2024-02-08 15:03:44 +0100

    ✨ feature: Nouveau Script SQl
    Creation des tables

--- Personnel/script/CreationTables.sql
+++ Personnel/script/CreationTables.sql
@@ -1,7 +1,26 @@
 /*
 Nom du script : CreationTables.sql
-Auteur : BldNicolas
-Date de création : 2024-01-21
+Auteur : Djony
+Date de création : 2024-01-02
 Objectif : Ce script contient les requêtes de création des tables nécessaires pour le projet AtelierProBts.
 */
 
+CREATE DATABASE IF NOT EXISTS Atelier_BTS;
+USE Atelier_BTS;
+SET SESSION DATE_FORMAT = "%d-%m-%Y";
+
+CREATE TABLE Ligue (
+    id_ligue INT PRIMARY KEY,
+    nom VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE Employe (
+    id_employe INT PRIMARY KEY,
+    droit VARCHAR(256) NOT NULL,
+    nom VARCHAR(256) NOT NULL,
+    prenom VARCHAR(256) NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    mail VARCHAR(256) NOT NULL,
+    date_arrive DATE NOT NULL,
+    date_depart DATE
+);
