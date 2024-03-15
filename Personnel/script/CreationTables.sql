80e32139a911916a0c5005a54bdbabfc52c41a9e
Nicolas <ni.berlaud@gmail.com>
2024-03-15 15:08:17 +0100

    🐛 fix: Asynchronisation de la création de la base de donnée

--- Personnel/script/CreationTables.sql
+++ Personnel/script/CreationTables.sql
@@ -1,23 +1,5 @@
-/*
-Nom du script : CreationTables.sql
-Auteur : Djony
-Date de création : 2024-01-02
-Objectif : Ce script contient les requêtes de création des tables nécessaires pour le projet VDNGestion.
-*/
-
 CREATE DATABASE IF NOT EXISTS VDNGestion;
 USE VDNGestion;
-
-/* Cette variable est seulement accessible en lecture dans mySQL donc impossible de le set de cette manière.
-*SET SESSION DATE_FORMAT = "%d-%m-%Y";
-*/
-
-CREATE TABLE Ligue (
-    id_ligue INT PRIMARY KEY AUTO_INCREMENT,
-    id_admin INT,
-    nom VARCHAR(256) NOT NULL,
-    FOREIGN KEY (id_admin) REFERENCES Employe (id_employe) 
-);
 
 CREATE TABLE Employe (
     id_employe INT PRIMARY KEY,
@@ -28,8 +10,15 @@ CREATE TABLE Employe (
     password VARCHAR(256) NOT NULL,
     mail VARCHAR(256) NOT NULL,
     date_arrive DATE NOT NULL,
-    date_depart DATE,
-    FOREIGN KEY (id_ligue) REFERENCES Ligue(id_ligue) 
+    date_depart DATE
 );
 
+CREATE TABLE Ligue (
+    id_ligue INT PRIMARY KEY AUTO_INCREMENT,
+    id_admin INT,
+    nom VARCHAR(256) NOT NULL,
+    FOREIGN KEY (id_admin) REFERENCES Employe (id_employe)
+);
 
+ALTER TABLE Employe
+ADD FOREIGN KEY (id_ligue) REFERENCES Ligue(id_ligue);
