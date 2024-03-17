7fc80ebbef8b620c50885534be471f6f902da541
Nicolas <ni.berlaud@gmail.com>
2024-03-17 17:41:24 +0100

    🐛 fix: JDBC Malfunction
    Temp commit until boss tell us if we have to edit the table script creation

--- Personnel/src/jdbc/JDBC.java
+++ Personnel/src/jdbc/JDBC.java
@@ -40,7 +40,7 @@ public class JDBC implements Passerelle
 			Statement instruction = connection.createStatement();
 			ResultSet ligues = instruction.executeQuery(requete);
 			while (ligues.next())
-				gestionPersonnel.addLigue(ligues.getInt(1), ligues.getString(2));
+				gestionPersonnel.addLigue(ligues.getInt(1), ligues.getString(3));
 		}
 		catch (SQLException e)
 		{
