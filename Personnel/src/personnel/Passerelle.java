67e1294ed36c415c0976ea7f1747d9e00a1eb4be
Lav000 <77846416+Lav000@users.noreply.github.com>
2024-03-20 10:39:55 +0100

    ✨ feature: adding insert employe in passerelle.java

--- Personnel/src/personnel/Passerelle.java
+++ Personnel/src/personnel/Passerelle.java
@@ -5,5 +5,6 @@ public interface Passerelle
 	public GestionPersonnel getGestionPersonnel();
 	public void sauvegarderGestionPersonnel(GestionPersonnel gestionPersonnel)  throws SauvegardeImpossible;
 	public int insertLigue(Ligue ligue) throws SauvegardeImpossible;
+	public int insertEmploye(Employe employe) throws SauvegardeImpossible;
 	public void removeLigue(Ligue ligue) throws SauvegardeImpossible;
 }
